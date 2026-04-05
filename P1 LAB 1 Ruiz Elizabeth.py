12525890f2c63b66715716a5f69d9b41d61374c1
ruize2039 <ruize2039@student.faytechcc.edu>
2026-04-05 17:07:00 -0400

    1st Python
    taking input from user,

---
+++ P1 LAB 1 Ruiz Elizabeth.py
@@ -0,0 +1,8 @@
+# Ruiz Elizabeth
+# P1 LAB 1
+# first assig
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+
+print("hello,",first_name,last_name +"!","Welcome to CTI-110!")
